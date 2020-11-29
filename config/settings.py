3c2382f0b389b191ec5657ceadd96b596e4eb711
Aleksander Malcew <amalcew@protonmail.com>
2020-11-29 15:52:10 +0100

    database backend changed from sqlite3 to postgres

--- config/settings.py
+++ config/settings.py
@@ -83,8 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amskills',
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
